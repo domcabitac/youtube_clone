f9c5f98d02201f70489c8f3c82b61a31b25a17f6
Dominic Cabitac <dominic.cabitac@uoit.net>
2022-10-02 20:28:12 -0400

    finished sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+import Sidebar from './Sidebar';
+
+const Feed = () => (
+  <Stack sx={{flexDirection:  {sx: "column", md: "row" }}}>
+    <Box sx={{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3D3D3D', px: {sx: 0, md: 2}}}>
+      <Sidebar />
+
+      <Typography classname="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff'}} >
+        Copyright 2022 Dom Cabitac
+      </Typography>
+    </Box>
+    <Box>
+      
+    </Box>
+  </Stack>
+)
 
 export default Feed
